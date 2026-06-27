1ab196d3b738c78f8083230c290d3536737f18ad
Joaquim Grabalosa <jgrabalosa.gest@gmail.com>
2026-06-27 12:09:32 +0200

    Configuracion inicial del proyecto Spring Boot

---
+++ src/main/java/com/joaquim/habitosapp/HabitosAppApplication.java
@@ -0,0 +1,13 @@
+package com.joaquim.habitosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitosAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitosAppApplication.class, args);
+    }
+
+}
